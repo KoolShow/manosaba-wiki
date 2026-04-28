ba036107a55c3b4011ad35644710fce7ec130583
Misa Liu <misaliu@misaliu.top>
2026-04-28 17:02:45 +0800

    chore(wiki): Add UnoCSS

--- packages/wiki/vite.config.ts
+++ packages/wiki/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite';
+import { presetWind4 } from 'unocss';
 import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    UnoCSS({
+      presets: [ presetWind4() ],
+    }),
+    preact(),
+  ],
 })
